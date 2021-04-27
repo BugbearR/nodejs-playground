8007757651066ea3046b1a62e429afba51746bd9
BugbearR <bugbearr@bugbearr.jp>
2021-04-27 02:56:27 +0000

    Add readline-exp

---
+++ readline-exp/index.js
@@ -0,0 +1,40 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+    prompt: ">"
+});
+
+rl.on("close", (line) => {
+    console.log("close Event.");
+});
+
+rl.on("line", (line) => {
+    console.log("line Event.");
+    console.log({line: line});
+});
+
+rl.on("history", (line) => {
+    console.log("history Event.");
+});
+
+rl.on("pause", (line) => {
+    console.log("pause Event.");
+});
+
+rl.on("resume", (line) => {
+    console.log("resume Event.");
+});
+
+rl.on("SIGCONT", (line) => {
+    console.log("SIGCONT Event.");
+});
+
+rl.on("SIGINT", (line) => {
+    console.log("SIGINT Event.");
+});
+
+rl.on("SIGTSTP", (line) => {
+    console.log("SIGTSTP Event.");
+});
