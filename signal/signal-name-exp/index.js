9298d2e2f962c6c1149eaedf1a157199c0ea93a0
BugbearR <bugbearr@bugbearr.jp>
2021-10-14 12:47:44 +0900

    Fix argv count check

--- signal/signal-name-exp/index.js
+++ signal/signal-name-exp/index.js
@@ -1,5 +1,5 @@
 #!/bin/env node
-if (process.argv.length < 2) {
+if (process.argv.length < 3) {
     console.log(`usage: ${process.argv[0]} ${process.argv[1]} signal_name`);
     process.exit(1);
 }
