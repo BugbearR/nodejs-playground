7d96bf0d78961a0f2b4266b8b73d506831394976
BugbearR <bugbearr@bugbearr.jp>
2021-10-28 10:49:53 +0900

    Add timetostring.js

---
+++ time/timetostring.js
@@ -0,0 +1,66 @@
+
+function toYYYYMMDD(t) {
+    return t.getFullYear().toString().padStart(4, '0')
+        + t.getMonth().toString().padStart(2, '0')
+        + t.getDate().toString().padStart(2, '0');
+}
+
+function toYYYYMMDDhhmm(t) {
+    return t.getFullYear().toString().padStart(4, '0')
+        + t.getMonth().toString().padStart(2, '0')
+        + t.getDate().toString().padStart(2, '0')
+        + t.getHours().toString().padStart(2, '0')
+        + t.getMinutes().toString().padStart(2, '0');
+}
+
+function toYYYYMMDDhhmmss(t) {
+    return t.getFullYear().toString().padStart(4, '0')
+        + t.getMonth().toString().padStart(2, '0')
+        + t.getDate().toString().padStart(2, '0')
+        + t.getHours().toString().padStart(2, '0')
+        + t.getMinutes().toString().padStart(2, '0')
+        + t.getSeconds().toString().padStart(2, '0');
+}
+
+function toYYYYMMDDhhmmssfff(t) {
+    return t.getFullYear().toString().padStart(4, '0')
+        + t.getMonth().toString().padStart(2, '0')
+        + t.getDate().toString().padStart(2, '0')
+        + t.getHours().toString().padStart(2, '0')
+        + t.getMinutes().toString().padStart(2, '0')
+        + t.getSeconds().toString().padStart(2, '0')
+        + t.getMilliseconds().toString().padStart(3, '0');
+}
+
+function toUTCYYYYMMDD(t) {
+    return t.getFullYear().toString().padStart(4, '0')
+        + t.getMonth().toString().padStart(2, '0')
+        + t.getDate().toString().padStart(2, '0');
+}
+
+function toUTCYYYYMMDDhhmm(t) {
+    return t.getUTCFullYear().toString().padStart(4, '0')
+        + t.getUTCMonth().toString().padStart(2, '0')
+        + t.getUTCDate().toString().padStart(2, '0')
+        + t.getUTCHours().toString().padStart(2, '0')
+        + t.getUTCMinutes().toString().padStart(2, '0');
+}
+
+function toUTCYYYYMMDDhhmmss(t) {
+    return t.getUTCFullYear().toString().padStart(4, '0')
+        + t.getUTCMonth().toString().padStart(2, '0')
+        + t.getUTCDate().toString().padStart(2, '0')
+        + t.getUTCHours().toString().padStart(2, '0')
+        + t.getUTCMinutes().toString().padStart(2, '0')
+        + t.getUTCSeconds().toString().padStart(2, '0');
+}
+
+function toUTCYYYYMMDDhhmmssfff(t) {
+    return t.getUTCFullYear().toString().padStart(4, '0')
+        + t.getUTCMonth().toString().padStart(2, '0')
+        + t.getUTCDate().toString().padStart(2, '0')
+        + t.getUTCHours().toString().padStart(2, '0')
+        + t.getUTCMinutes().toString().padStart(2, '0')
+        + t.getUTCSeconds().toString().padStart(2, '0')
+        + t.getUTCMilliseconds().toString().padStart(3, '0');
+}
