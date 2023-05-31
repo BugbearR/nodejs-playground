5be117be10c0c5b6de3fe10713615119f859639f
BugbearR <bugbearr@bugbearr.jp>
2023-05-31 06:07:42 +0000

    Add isValid, calcCheckDigit method

--- modulus43/modulus43.js
+++ modulus43/modulus43.js
@@ -9,8 +9,8 @@ export class Modulus43
 
     /**
      * encode modulus43
-     * @param {string} c character
-     * @returns number
+     * @param {string} n number
+     * @returns character
      */
     static encode(n)
     {
@@ -25,5 +25,31 @@ export class Modulus43
     static decode(c)
     {
         return MODULUS43_CHAR_TABLE.indexOf(c);
+    }
+
+    /**
+     * validate code39 string
+     * @param {string} s code39 string
+     * @returns {boolean} true if the string is a valid Code39 string, false otherwise
+     */
+    static isValid(s)
+    {
+        return Array.from(s).every((c) => {
+            return this.decode(c) >= 0;
+        });
+    }
+
+    /**
+     * calculate check digit (modulus43)
+     * @param {string} s code39 string (check digit is not included)
+     * @returns check digit (modulus43)
+     */
+    static calcCheckDigit(s)
+    {
+        const sumCodes = Array.from(s).reduce((acc, c) => {
+            return acc + this.decode(c);
+        }, 0);
+        const checkDigitNum = sumCodes % 43;
+        return this.encode(checkDigitNum);
     }
 }
