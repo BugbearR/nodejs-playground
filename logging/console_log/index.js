cb02b86d8547ebdaf6c60c36a86475d029344b8c
BugbearR <bugbearr@bugbearr.jp>
2023-11-09 07:32:42 +0000

    Add console logging and log4js sample

---
+++ logging/console_log/index.js
@@ -0,0 +1,20 @@
+console.log("console.log");
+console.info("console.info");
+console.warn("console.warn");
+console.error("console.error");
+console.debug("console.debug");
+console.trace("console.trace");
+
+console.log({n:1, s:"log", a:[1,"abc"], o:{n:1, s:"abc"}});
+console.info({n:1, s:"info", a:[1,"abc"], o:{n:1, s:"abc"}});
+console.warn({n:1, s:"warn", a:[1,"abc"], o:{n:1, s:"abc"}});
+console.error({n:1, s:"error", a:[1,"abc"], o:{n:1, s:"abc"}});
+console.debug({n:1, s:"debug", a:[1,"abc"], o:{n:1, s:"abc"}});
+console.trace({n:1, s:"trace", a:[1,"abc"], o:{n:1, s:"abc"}});
+
+console.log("log msg", {n:1, s:"abc", a:[1,"abc"], o:{n:1, s:"abc"}});
+console.info("info msg", {n:1, s:"abc", a:[1,"abc"], o:{n:1, s:"abc"}});
+console.warn("warn msg", {n:1, s:"abc", a:[1,"abc"], o:{n:1, s:"abc"}});
+console.error("error msg", {n:1, s:"abc", a:[1,"abc"], o:{n:1, s:"abc"}});
+console.debug("debug msg", {n:1, s:"abc", a:[1,"abc"], o:{n:1, s:"abc"}});
+console.trace("trace msg", {n:1, s:"abc", a:[1,"abc"], o:{n:1, s:"abc"}});
