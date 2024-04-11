28bb67272b775208847148c20033b5ecb8c649c9
BugbearR <bugbearr@bugbearr.jp>
2024-04-11 17:07:08 +0900

    Add sample executable script

--- child_process/spawn-exp/test.sh
+++ child_process/spawn-exp/test.sh
@@ -1,3 +1,14 @@
 #!/bin/sh
 
-echo "Hello!"
+for i in $(seq 1 10)
+do
+  case $(( $(od -vAn -N1 -tu1 </dev/random) % 2)) in
+  1)
+    echo "Hello $i"
+    ;;
+  0)
+    echo "Error $i" 1>&2
+    ;;
+  esac
+  sleep 1
+done
